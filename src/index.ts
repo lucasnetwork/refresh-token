0918d32fbdcf799e23d33ae26910bbd781351e09
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2021-12-21 14:58:45 -0300

    initial database

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,21 @@
-import express,{Express} from 'express'
-import routes from './routes'
+import "reflect-metadata";
+import {createConnection} from "typeorm";
+import {User} from "./entity/User";
 
-class App{
+createConnection().then(async connection => {
 
-    app:Express
+    console.log("Inserting a new user into the database...");
+    const user = new User();
+    user.firstName = "Timber";
+    user.lastName = "Saw";
+    user.age = 25;
+    await connection.manager.save(user);
+    console.log("Saved a new user with id: " + user.id);
 
-    constructor(){
-        this.app = express()
-        this.route()
-    }
+    console.log("Loading users from the database...");
+    const users = await connection.manager.find(User);
+    console.log("Loaded users: ", users);
 
-    route(){
-        this.app.use(routes)
-    }
-}
+    console.log("Here you can setup and run express/koa/any other framework.");
 
-export default App
+}).catch(error => console.log(error));
